0091cd5e9efad14d50f520f7dafbee21590243e6
OpenSauce04 <opensauce04@gmail.com>
2022-12-05 17:39:19 +0000

    Completed Day 1

--- AdventOfCode2022/Day1/Program.cs
+++ AdventOfCode2022/Day1/Program.cs
@@ -1,10 +1,25 @@
-﻿namespace Day1
+﻿using System;
+using System.IO;
+using System.Reflection;
+namespace Day1
 {
 	internal class Program
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			int highCalories = 0;
+			int runningCalories = 0;
+			foreach (string x in File.ReadAllLines(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/input.txt")) {
+				if (x == "")
+				{
+					runningCalories = 0;
+				} else
+				{
+					runningCalories += Int32.Parse(x.Trim());
+					highCalories = Math.Max(runningCalories, highCalories);
+				}
+			}
+			Console.WriteLine(highCalories);
 		}
 	}
 }
