2a92e86eefd92aaea0248b36f119a7fbac557396
OpenSauce04 <opensauce04@gmail.com>
2022-12-05 18:13:47 +0000

    Completed first part of Day 2

---
+++ AdventOfCode2022/Day2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.IO;
+using System.Reflection;
+using System;
+
+namespace Day2
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			ArrayList moveList = new ArrayList();
+			int score = 0;
+
+			foreach (string x in File.ReadAllLines(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "/input.txt"))
+			{
+				moveList.Add(x.Split(' '));
+			}
+
+			foreach (string[] x in moveList)
+			{
+				switch (x[1])
+				{
+					case "X":
+						score += 1;
+						if (x[0] == "A") score += 3;
+						if (x[0] == "C") score += 6;
+						break;
+					case "Y":
+						score += 2;
+						if (x[0] == "B") score += 3;
+						if (x[0] == "A") score += 6;
+						break;
+					case "Z":
+						score += 3;
+						if (x[0] == "C") score += 3;
+						if (x[0] == "B") score += 6;
+						break;
+				}
+			}
+
+			Console.WriteLine(score);
+		}
+	}
+}
